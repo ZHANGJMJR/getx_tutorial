dee19f4499494fefa0ebb64134f4ee76e6701241
zhangjmjr <zhangjmjr@gmail.com>
2024-02-22 21:30:56 +0800

    first commit for blank architecture Getx App

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Getx Architecture',
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.fade,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
